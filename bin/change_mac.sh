d9b1bf4bffbe65a0884e96743bba66e1ad68cb11
chunyu1202 <chunyu1202@gmail.com>
2019-05-02 16:59:38 +0800

    Change onboard bluetooth mac address

---
+++ bin/change_mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MAC_PREFIX="C1:"
+MAC_SUFFIX=`sudo hciconfig hci0 | grep "BD Address" | cut -d ":" -f 3-7 | cut -d " " -f 1`
+sudo hciconfig hci0 up
+sleep 3
+sudo /home/pi/bdaddr/bdaddr -i hci0 -r $MAC_PREFIX$MAC_SUFFIX
+sudo hciconfig hci0 reset
+sudo systemctl restart bluetooth.service
+sleep 3
+sudo hciconfig hci0 up
+
+exit 0
